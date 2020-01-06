d75a0123620ae9470eca7b7e977249cde0abde6d
bonnie.hung <bonnie.hung@accenture.com>
2020-01-06 15:13:49 -0500

    intialized scene, renderer, and camera

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 (function(){
-  //main variables
+  //Main variables
   let scene,
       renderer,
       camera,
@@ -13,5 +13,42 @@
       currentlyAnimating = false,
       raycaster = new THREE.Raycaster(),    //detect click on the character (model)
       loaderAnim = document.getElementById('js-loader');
+
+      init();
+
+      function init(){
+        const canvas = document.querySelector('c');
+        const backgroundColor = 0xf1f1f1; //threejs references colours in a hexadecimal integer
+
+        /** Initialize scene **/
+
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(backgroundColor);
+        //not that visible, but if floor and background are different, it can come in handy to blur those together
+        scene.fog = new THREE.Fog(backgroundColor, 60, 100);
+
+        /** Initialize the renderer **/
+
+        renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+        //enable shadowMap so character will cast a shadow
+        renderer.shadowMap.enabled = true;
+        //set pixel ratio to that of device so mobile devices render correctly (otherwise canvas will display pixelated on high density screens))
+        renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(renderer.domElement);
+
+        /** Add camera **/
+
+        camera = new THREE.PerspectiveCamera(
+          //fov, aspect ratio, near and far clipping plane
+          50,
+          window.innerWidth/window.innerHeight,
+          0.1,
+          1000
+        );
+        camera.position.z = 30;
+        camera.position.x = 0;
+        camera.position.y = -3;
+
+      }
 
 })();
